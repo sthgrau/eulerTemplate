1e458630afb52fddee62d015690c6dc535c8c66a
sthgrau <sthgrau@gmail.com>
2017-09-02 21:44:29 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Someone suggested to me that I should do the Euler Project problems from https://projecteuler.net/ in order to keep my python knowledge fresh.
+
+I wanted to make aprogram to grab the problem descriptions and create files with the problem description as a comment. Right now, I have only done python, but other languages shouldn't be a problem.
+
+To use:
+
+* clone this repository
+* create python subdirectory
+* run the get_problems.py script
---
+++ get_problem.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+from HTMLParser import HTMLParser
+import urllib2
+import sys
+import re
+import time
+from os import listdir
+from os import path
+
+class myParser(HTMLParser):
+  inProblem=False
+  problemDepth=0
+  problemData=""
+  def handle_data(self, data):
+    if self.inProblem:
+      self.problemData += data
+  def handle_endtag(self, tag):
+    if tag == "div":
+      if self.inProblem:
+        self.problemDepth -= 1
+      if self.problemDepth == 0:
+        self.inProblem=False
+  def handle_startendtag(self, tag,attrs):
+    if tag == "br" and self.inProblem:
+      self.problemData += " \n"
+  def handle_starttag(self, tag, attrs):
+    stuff = "<%s " % (tag)
+    for myattr,myval in attrs:
+      stuff += "%s='%s' " % (myattr,myval)
+    stuff += ">"
+    if tag == "div":
+      if self.inProblem:
+        self.problemDepth += 1
+      for myattr,myval in attrs:
+        if myattr == "class" and myval == "problem_content":
+          self.inProblem=True
+          self.problemDepth += 1
+    if tag == "span" and self.inProblem:
+      self.problemData += stuff
+    if tag == "sup" and self.inProblem:
+      self.problemData += "**"
+    if tag == "br" and self.inProblem:
+      self.problemData += "\n"
+    if tag == "a" and self.inProblem:
+      self.problemData += stuff.replace("href='","href='https://projecteuler.net/")
+    if tag == "img" and self.inProblem:
+      self.problemData += stuff.replace("src='","src='https://projecteuler.net/")
+        
+
+URLTEMPLATE="https://projecteuler.net/problem="
+DEBUG0=False
+PROBLEMDIR="problem_descriptions"
+
+def getProblemDescription(problemId):
+  try:
+    f = urllib2.urlopen("%s%s" % (URLTEMPLATE,problemId))
+    
+    fs = f.read()
+    f.close()
+    p = myParser()
+    p.feed(fs)
+    myresult = p.problemData 
+    myfile = open("%s/problem%s.txt" % (PROBLEMDIR,problemId),"w")
+    myfile.write(myresult)
+    myfile.close()
+    return True
+  except:
+    e = sys.exc_info()[0]
+    if DEBUG0: print "error = %s" % (e)
+    return False
+
+def getProblems(max=700):
+  ps = sorted([int(re.search("problem([0-9]*).txt",x).group(1)) for x in listdir(PROBLEMDIR)])
+  if len(ps) > 0:
+    last=ps[-1]
+  else:
+    last=0
+  next=last + 1
+  print next
+  while getProblemDescription(next) and next < max:
+    time.sleep(2)
+    next=next + 1
+  problemId=1
+  if path.isdir("python"):
+    while path.isfile("%s/problem%s.txt" % (PROBLEMDIR,problemId)):
+      dst="python/%s.py" % (problemId)
+      src="%s/problem%s.txt" % (PROBLEMDIR,problemId)
+      if not path.isfile(dst):
+        y = open(src,"r")
+        ys = y.read()
+        y.close()
+        x = open(dst, "w")
+        x.write("""#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+\"\"\"
+%s
+\"\"\"
+
+import sys
+if __name__ == '__main__':
+    sys.exit(0)
+""" %(ys))
+
+        x.close()
+      problemId+=1
+
+
+if __name__ == '__main__':
+  if len(sys.argv) > 1:
+    max=sys.argv[1]
+  else:
+    max=20
+  getProblems(max)
+  sys.exit(0)
